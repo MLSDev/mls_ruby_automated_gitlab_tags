7715988b5d2491427d3d49db3d31b578e4da8c2e
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2019-05-06 17:04:16 +0300

    dont force to use rails-ujs and activestorage (rails 5 stuff)

--- app/assets/javascripts/mls_ruby_automated_gitlab_tags/application.js
+++ app/assets/javascripts/mls_ruby_automated_gitlab_tags/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
+// require rails-ujs - removed
+// require activestorage - removed
 //= require_tree .
